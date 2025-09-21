04a23d5dc000f1184f4b8653ae4dc53ef2d05beb
laradelcoro24 <laradelcoro01@gmail.com>
2025-09-21 13:49:30 -0300

    agrego validaciones en pagos

--- proyFinal/src/controllers/pagosController.py
+++ proyFinal/src/controllers/pagosController.py
@@ -2,6 +2,7 @@ import datetime
 from flask import current_app, render_template, url_for
 from flask_login import current_user
 from flask_mail import Message
+import pytz
 
 from src.models.pago import Pago
 from src.models.usuario import Usuario
@@ -11,7 +12,7 @@ from src import db
 from src.utils.Mail import mail
 from sqlalchemy import or_, and_
 
-def obtener_pagos(estado= None,  fecha_desde=None, fecha_hasta=None):
+def obtener_pagos(estado= None, fecha_desde=None, fecha_hasta=None):
     query = Pago.query
 
     if current_user.IdRol == RolEnum.Deportista:
@@ -35,7 +36,7 @@ def obtener_pagos(estado= None,  fecha_desde=None, fecha_hasta=None):
 
     # Filtrar por fecha hasta
     if fecha_hasta:
-        fecha_hasta_dt = datetime.datetime.strptime(fecha_hasta, "%Y-%m-%d")
+        fecha_hasta_dt = datetime.datetime.strptime (fecha_hasta, "%Y-%m-%d")
         query = query.filter(
             or_(
                 and_(Pago.FechaPago != None, Pago.FechaPago <= fecha_hasta_dt),
@@ -92,3 +93,50 @@ def borrar_pago(pago):
     db.session.delete(pago)
     db.session.commit()
  
+ 
+def enviar_recordatorios_cuotas():
+    enviados = 0
+    arg = pytz.timezone("America/Argentina/Buenos_Aires")
+    hoy = datetime.now(arg).date()
+
+    # Buscar pagos vencidos sin fecha de pago y que no estén marcados como Pagados
+    pagos_vencidos = (
+        Pago.query
+        .filter(
+            Pago.FechaVencimiento < hoy,
+            Pago.FechaPago.is_(None),
+            Pago.IdEstado != generalEnum.EstadoPagoEnum.Pago.value
+        )
+        .all()
+    )
+
+    for pago in pagos_vencidos:
+        deportista = Usuario.query.get(pago.IdUsuario)
+        if enviar_recordatorio_individual(deportista, pago):
+            enviados += 1
+    
+    print(f"[INFO] Recordatorios enviados: {enviados}")
+    return enviados
+
+def enviar_recordatorio_individual(deportista, pago):
+    if not deportista or not deportista.Email:
+        return False
+    
+    msg = Message(
+        subject="Voley App - Recordatorio de Cuota Vencida",
+        sender=current_app.config['MAIL_USERNAME'],
+        recipients=[deportista.Email]
+    )
+    msg.html = render_template(
+        "pago/recordatorioCuota.html",
+        deportista=deportista,
+        fecha_venc=pago.FechaVencimiento.strftime("%d/%m/%Y")
+    )
+
+    try:
+        mail.send(msg)
+        print(f"[INFO] Email enviado a {deportista.Email}")
+        return True
+    except Exception as e:
+        print(f"[ERROR] No se pudo enviar el correo a {deportista.Email}: {e}")
+        return False
