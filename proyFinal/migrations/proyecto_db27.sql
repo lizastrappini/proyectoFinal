c72284acf5b2a04f07a8f8e502016c98415a7c74
lizastrappini <lizastrappini99@gmail.com>
2025-10-05 21:44:19 -0300

    campo rutaArchivo

--- proyFinal/migrations/proyecto_db27.sql
+++ proyFinal/migrations/proyecto_db27.sql
@@ -41,7 +41,7 @@ CREATE TABLE `EstadisticaPorPartido` (
   `Resultado` int NOT NULL,
   `FechaCarga` datetime NOT NULL,
   `IdEntrenador` int NOT NULL,
-  `RutaArchivo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+  `RutaArchivo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
